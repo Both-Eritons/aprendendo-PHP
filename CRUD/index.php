29dc359eee472d119a6bda3b83e73bd650066fa1
Both-Eritons <both-eritons@outlook.com>
2022-06-12 18:10:22 -0300

    CRUD && CONNECT MYSQL ETC

---
+++ CRUD/index.php
@@ -0,0 +1,29 @@
+<?php include_once 'includes/header.php';?>
+<div class="row">
+	<div class="col s12 m6 push-m3">
+		<h3 class="light center text-bold">clientes</h3>
+		<table class="striped">
+			<thead>
+				<tr>
+					<th>Nome</th>
+					<th>sobre nome</th>
+					<th>Email</th>
+					<th>idade</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>eriton</td>
+					<td>junior</td>
+					<td>eriton@gmail.com</td>
+					<td>18</td>
+					<td><a href="" class="btn-floating orange"><i class="material-icons">edit</i>
+					<td><a href="" class="btn-floating red"><i class="material-icons">delete</i>
+				</tr>
+			</tbody>
+		</table>
+		</br>
+		<a href="adicionar.php" class="btn btn-primary">Adicionar cliente</a>
+	</div>
+</div>
+<?php include_once 'includes/footer.php'; ?>     
---
+++ CRUD/php_action/db_connect.php
@@ -0,0 +1,9 @@
+<?php
+//conexao
+$servername = '127.0.0.1';
+$username = 'root';
+$password = '12345678';
+$db_name = 'crud';
+
+$connect = mysqli_connect($servername, $username, $password,$db_name);
+
