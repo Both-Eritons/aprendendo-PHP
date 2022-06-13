8648c37f2a05416d2b501a8de27a4d0065444c72
Both-Eritons <both-eritons@outlook.com>
2022-06-13 17:56:03 -0300

    XSS

--- CRUD/php_action/create.php
+++ CRUD/php_action/create.php
@@ -2,11 +2,18 @@
 session_start();
 //conexao
 require_once 'db_connect.php';
+function clear($input){
+	global $connect;
+	$var = mysqli_escape_string($connect, $input);
+	$var = htmlspecialchars($var);
+
+	return $var;
+}
 if(isset($_POST['btn_cadastrar'])){
-	$nome = mysqli_escape_string($connect, $_POST['nome']);
-	$sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
-	$email = mysqli_escape_string($connect, $_POST['email']);
-	$idade = mysqli_escape_string($connect, $_POST['idade']);
+	$nome = clear($_POST['nome']);
+	$sobrenome = clear($_POST['sobrenome']);
+	$email = clear($_POST['email']);
+	$idade = clear($_POST['idade']);
 	$sql = "INSERT INTO clientes (nome, sobrenome, email, idade) VALUES ('$nome', '$sobrenome', '$email', '$idade')";
 	if(mysqli_query($connect,$sql)){
 		$_SESSION['mensagem'] = "atualizado com sucesso!";
