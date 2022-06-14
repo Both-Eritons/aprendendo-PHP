df05b5e564e266b33f148eab56d02f43eadcab30
Both-Eritons <both-eritons@outlook.com>
2022-06-14 17:07:06 -0300

    cookie

---
+++ cookie/index.php
@@ -0,0 +1,8 @@
+<?php
+//COOKIE
+
+
+setcookie('user', 'eriton santos', time()+3600);
+setcookie('email', 'eriton@gmail.com', time()+3600);
+setcookie('ultima_pesquisa', 'tenis adidas', time()-3600);//nao existe
+
