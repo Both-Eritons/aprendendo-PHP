d70ecdca4876b52377b4ee785296439c33a24142
Both-Eritons <both-eritons@outlook.com>
2022-06-15 13:33:44 -0300

    MVC

---
+++ MVC/src/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '09f7f182d1676f44eb083299c2b572297d9c86ee',
+        'name' => 'eriton/mvc',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'eriton/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '09f7f182d1676f44eb083299c2b572297d9c86ee',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.2',
+            'version' => '6.6.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'b52ed06864fdda81b82ec8bf564cf15d45ed4f95',
+            'dev_requirement' => false,
+        ),
+    ),
+);
